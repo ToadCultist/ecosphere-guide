fbffe5c42e7c03fc20279fc847195c6688b7639b
skull4L <lucaschicksand@gmail.com>
2024-10-12 22:30:01 +0100

    branding

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/ToadCultist/ecosphere-guide",
+      "Enclosed Ecology Discord server": "https://discord.gg/enclosed-ecology-969887747311566860",
     },
   }),
 }
