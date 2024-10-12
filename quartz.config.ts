0d8b3d0389052c4cd9d3f9274ad36049923bb017
skull4L <lucaschicksand@gmail.com>
2024-10-12 18:48:07 +0100

    help

--- quartz.config.ts
+++ quartz.config.ts
@@ -77,11 +77,11 @@ const config: QuartzConfig = {
       Plugin.ContentPage(),
       Plugin.FolderPage(),
       Plugin.TagPage(),
-      Plugin.Assets(),
       Plugin.ContentIndex({
         enableSiteMap: true,
         enableRSS: true,
       }),
+      Plugin.Assets(),
       Plugin.Static(),
       Plugin.NotFoundPage(),
     ],
